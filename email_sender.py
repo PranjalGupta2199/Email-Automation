57ccc2b3917fcb49da21d6d62d2996e9096f34e0
Pranjal Gupta <pranjalgupta2199@gmail.com>
2018-09-16 22:20:52 +0530

    Sends attachment

--- email_sender.py
+++ email_sender.py
@@ -3,8 +3,12 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.encoders import encode_base64
 import base64
-import csv
+import mimetypes
+import os
 
 def create_message(sender, to, subject, message_text):
     """Create a message for an email.
@@ -18,10 +22,65 @@ def create_message(sender, to, subject, message_text):
     Returns:
         An object containing a base64url encoded email object.
     """
-    message = MIMEText(message_text)
+    message = MIMEText(message_text,'html')
     message['to'] = to
     message['from'] = sender
     message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode())}
+
+def create_message_with_attachment(
+    sender, to, subject, message_text, file):
+    """Create a message for an email.
+
+      Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+        file: The path to the file to be attached.
+
+      Returns:
+        An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text, 'html')
+    message.attach(msg)
+
+    content_type, encoding = mimetypes.guess_type(file)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(file, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(file, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(file, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    
+    filename = os.path.basename(file)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    encode_base64(msg)
+    message.attach(msg)
+
+
+
+    
     return {'raw': base64.urlsafe_b64encode(message.as_string().encode())}
 
 
@@ -37,15 +96,14 @@ def send_message(service, user_id, message):
     Returns:
         Sent Message.
     """
-    for keys in message : 
-        message[keys] = message[keys].decode('utf-8')
     
+    message['raw'] = message['raw'].decode('utf-8')    
     try:
         message = (service.users().messages().send(userId=user_id, body=message).execute())
         #print('Message Id: %s' % message['id'])
         return message
-    except errors.HttpError as error:
-        print('An error occurred: %s' % error)
+    except :
+        print('An error occurred')
 
 
 
@@ -66,6 +124,9 @@ def send_mail(mail):
     reciever = mail['reciever']
     subject = mail['subject']
     msg_text = mail['body']
-    msg = create_message(sender, reciever, subject, msg_text)
-    print (type(msg))
+    attach = mail['file']
+
+    #msg = create_message(sender, reciever, subject, msg_text)
+    msg = create_message_with_attachment(sender, reciever, subject, msg_text, attach) 
+    #print (type(msg))
     send_message(service, "me", msg)
